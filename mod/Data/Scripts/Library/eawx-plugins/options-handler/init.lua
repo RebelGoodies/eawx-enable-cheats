55ba1a9473c2357b44678a26bb648aa30780dba4
rebelgoodies <rebelgoodies@users.noreply.github.com>
2026-01-03 13:04:58 -0700

    Update EaWX upstream to TR 3.5

--- mod/Data/Scripts/Library/eawx-plugins/options-handler/init.lua
+++ mod/Data/Scripts/Library/eawx-plugins/options-handler/init.lua
@@ -5,8 +5,6 @@ return {
     type = "plugin",
     target = PluginTargets.never(),
     init = function(self, ctx)
-        local galactic_conquest = ctx.galactic_conquest
-        return OptionsHandler(galactic_conquest, galactic_conquest.HumanPlayer)
+        return OptionsHandler(ctx.galactic_conquest, ctx.id)
     end
 }
- 
